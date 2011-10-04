12e2104b9f909aebb25cd0f074d15c41451f29d4
Matt Fowles <matt.fowles@gmail.com>
2011-10-04 18:07:10 -0400

    Fix a warning in the javadoc

--- java-src/com/streambase/sbunit/ext/matchers/IgnoreNullTransform.java
+++ java-src/com/streambase/sbunit/ext/matchers/IgnoreNullTransform.java
@@ -5,11 +5,12 @@ import com.streambase.sbunit.ext.ValueMatcher;
 /**
  * Does this {@link ValueMatcher} require structural recursion 
  * when instructed to ignore nulls values.
- * 
- * @return an {@link ValueMatcher} like <code>this</code> but that will
- * ignore sub-values that are null.  If the result would be a trivial
- * {@link ValueMatcher}, return <code>null</code> instead.
  */
 public interface IgnoreNullTransform extends ValueMatcher {
+	/**
+	 * @return an {@link ValueMatcher} like <code>this</code> but that will
+	 * ignore sub-values that are null.  If the result would be a trivial
+	 * {@link ValueMatcher}, return <code>null</code> instead.
+	 */
     public ValueMatcher ignoreNulls();
 }
