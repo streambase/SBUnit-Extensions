00f70258f4959401e3417eafcd63cddb1cc601f8
Matt Fowles <matt.fowles@gmail.com>
2011-10-10 17:41:28 -0400

    add back in a make-pages script

---
+++ make-pages.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# build the relevant javadoc and test stuff
+# then push it over to the git-hub pages section
+
+VERSION=`hg bookmarks | grep '*' | cut -c4-6`
+echo Prepping for $VERSION
+mkdir -p $VERSION
+ant clean
+ant
+hg up gh-pages
+cp -r build/java-doc $VERSION
+cp -r build/test-doc $VERSION
+hg addremove
+hg ci -m 'updating the java doc and test-doc'
+hg up $VERSION
