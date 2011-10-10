a44f2564eead7d5c57773233efb48f2922ee66f1
Matt Fowles <matt.fowles@gmail.com>
2011-10-10 17:43:23 -0400

    update build script

--- make-pages.sh
+++ make-pages.sh
@@ -3,9 +3,14 @@
 # build the relevant javadoc and test stuff
 # then push it over to the git-hub pages section
 
+VERSION=`hg bookmarks | grep '*' | cut -c4-6`
+echo Prepping for $VERSION
+mkdir -p $VERSION
+ant clean
 ant
 hg up gh-pages
-cp -r build/java-doc .
-cp -r build/test-doc .
-hg ci -m 'updating the java doc and test-doc'
-hg up master
+cp -r build/java-doc $VERSION
+cp -r build/test-doc $VERSION
+hg addremove
+hg ci -m "updating the java doc and test-doc for version $VERSION"
+hg up $VERSION
