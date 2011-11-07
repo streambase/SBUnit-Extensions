8dc8350bcdb7b7fee78db59ac984d4a2abbdf04d
Matt Fowles <matt.fowles@gmail.com>
2011-11-07 13:26:36 -0500

    check return code
    --HG--
    extra : transplant_source : %C9%80x%E5%18%9F%20%0C%0AN%BE-%26%7C%A2%EAP%3A%0E%8E

--- make-pages.sh
+++ make-pages.sh
@@ -6,11 +6,12 @@
 VERSION=`hg bookmarks | grep '*' | cut -c4-6`
 echo Prepping for $VERSION
 mkdir -p $VERSION
-ant clean
-ant
-hg up gh-pages
-cp -r build/java-doc $VERSION
-cp -r build/test-doc $VERSION
-hg addremove
-hg ci -m 'updating the java doc and test-doc'
-hg up $VERSION
+ant clean && \
+ant && \
+hg up gh-pages && \
+cp -r build/java-doc $VERSION && \
+cp -r build/test-doc $VERSION && \
+hg addremove && \
+hg ci -m "updating the java doc and test-doc for version $VERSION" && \
+hg up $VERSION && \
+echo "Successfully updated the doc for $VERSION"
